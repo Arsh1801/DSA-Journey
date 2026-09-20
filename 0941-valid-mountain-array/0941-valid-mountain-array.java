cb6f7c10ae00205b1e632cb7ecea7d6f498416a2
Arsh Ashish Saxena <saxena.arshashish10@gmail.com>
2026-09-21 02:06:20 +0530

    Time: 1 ms (99.91%), Space: 47.7 MB (17.08%) - LeetHub

---
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length<3)return false;
+        int i = 1;
+        while(i<arr.length && arr[i]>arr[i-1]) i++;
+        if(i==1 || i == arr.length) return false;
+        while(i<arr.length && arr[i]<arr[i-1]) i++;
+        return i == arr.length;
+    }
+}
