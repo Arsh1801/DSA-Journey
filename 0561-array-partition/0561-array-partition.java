58885dbf55720f2afa88c238c1258d47d2a9558e
Arsh Ashish Saxena <saxena.arshashish10@gmail.com>
2026-09-23 13:28:29 +0530

    Time: 17 ms (79.74%), Space: 49.8 MB (35.07%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,7 +1,7 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        Arrays.sort(nums);
         int sum = 0;
+        Arrays.sort(nums);
         for(int i=0;i<nums.length;i+=2){
             sum+= nums[i];
         }
