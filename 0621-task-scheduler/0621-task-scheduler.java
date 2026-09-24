c7803f55ad450735c788beefe50a18153ee51fa8
Arsh Ashish Saxena <saxena.arshashish10@gmail.com>
2026-09-25 00:02:05 +0530

    Time: 31 ms (24.93%), Space: 48.5 MB (22.45%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] freq = new int[26];
+        for (char task : tasks) {
+            freq[task - 'A']++;
+        }
+        PriorityQueue<Integer> pq =
+            new PriorityQueue<>(Collections.reverseOrder());
+        for (int count : freq) {
+            if (count > 0) {
+                pq.add(count);
+            }
+        }
+        int time = 0;
+        while (!pq.isEmpty()) {
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i <= n; i++) {
+                if (!pq.isEmpty()) {
+                    int count = pq.poll();
+                    count--;
+                    if (count > 0) {
+                        temp.add(count);
+                    }
+                }
+                time++;
+                if (pq.isEmpty() && temp.isEmpty()) {
+                    break;
+                }
+            }
+            for (int count : temp) {
+                pq.add(count);
+            }
+        }
+        return time;
+    }
+}
